137cdec4b5f119d6c81991bc506254cb152a0e1b
koonut <koonut.thongchang@mca.christuniversity.in>
2022-11-19 10:47:38 +0530

    Lab3AndMemeGen

--- main.js
+++ main.js
@@ -6,14 +6,14 @@ const memeAuthor =document.querySelector(".meme-generator .meme-author");
 const updateDetails=(url,title,author) =>{
     memeImage.setAttribute("src",url);
     memeTitle.innerHTML=title;
-    memeAuthor.innerHTML='Meme by: {author}';
+    memeAuthor.innerHTML=author;
 };
 const generateMeme=()=>
 {
     fetch(" https://meme-api.herokuapp.com/gimme/wholesomememes")
     .then((Response)=> Response.json())
-    .then(data=>
-        {updateDetails(data.url, data.title, data.author)
+    .then((data) => {
+        updateDetails(data.url, data.title, data.author);
     });
 };
 generateMemeBtn.addEventListener("click",generateMeme);
